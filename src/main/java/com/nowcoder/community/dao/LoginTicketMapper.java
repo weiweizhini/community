44c2fccb085b6d4a911ef9db29768a2079514711
zhangkaiwei <370710180@qq.com>
2020-03-23 10:27:04 +0800

    完成社区登录模块注册会话登录账号设置检查状态

---
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface LoginTicketMapper {
+    //三个方法
+    //插入
+    @Insert({
+            "insert into login_ticket(user_id,ticket,status,expired) ",
+            "values(#{userId},#{ticket},#{status},#{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    //查找
+    @Select({
+            "select id,user_id,ticket,status,expired " ,
+            "from login_ticket where ticket=#{ticket}"
+
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    //更新
+    @Update({
+            "<script>",
+            "update login_ticket set status=#{status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\"> ",
+            "and 1=1 ",
+            "</if>",
+            "</script>"
+    })
+
+    int updateStatus(String ticket, int status);
+}
