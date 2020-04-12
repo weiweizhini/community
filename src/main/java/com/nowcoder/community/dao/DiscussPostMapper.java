2a9d940f25d17baa43615a1520a21adfcf562750
zhangkaiwei <370710180@qq.com>
2020-04-12 23:22:13 +0800

    完成核心功能 性能存储

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -14,6 +14,9 @@ public interface DiscussPostMapper {
     //@Parm注解用于给参数起别名  如果只有一个参数，并且在<if>里使用，则必须加别名
     int selectDiscussPostRows(@Param("userId") int userId);
 
+    int insertDiscussPost(DiscussPost discussPost);
 
+    DiscussPost selectDiscussPostById(int id);
 
+    int updateCommentCount(int id, int commentCount);
 }
--- src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -4,6 +4,7 @@ import com.nowcoder.community.entity.LoginTicket;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
+@Deprecated
 public interface LoginTicketMapper {
     //三个方法
     //插入
